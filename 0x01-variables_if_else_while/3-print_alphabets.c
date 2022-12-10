c8a03d5c80ef0dd328207c8942f4ac593e6d7b27
samecj <chijiokesamuel6@gmail.com>
2022-12-10 05:29:44 -0800

    commiteed after errrorr

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,11 +10,10 @@ int main(void)
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 
-	for (ch = 'A'; ch <= 'Z'; ch++)
+	for (ch = 'Á'; ch <= 'Z'; ch++)
 		putchar(ch);
 
 
 	putchar('\n');
 
-	return (0);
-}
+	return (0);}
