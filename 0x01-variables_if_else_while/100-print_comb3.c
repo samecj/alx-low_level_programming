bb5afe9e8430ce48fd41cc3ac723c5f334b9f16a
samecj <chijiokesamuel6@gmail.com>
2022-12-11 13:53:04 -0800

    commited second advance

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,3 +1,27 @@
 #include <stdio.h>
 /**
- * main - 
+ * main -t be separated by ,
+ * Return: Always 0
+ */
+int main(void)
+{
+int x = 1, y = 2;
+
+while ((x <= 8) && (y <= 9))
+{
+	putchar(x + '0');
+	putchar(y + '0');
+	if (x == 8)
+	{
+		putchar(' ');
+	}
+	else
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	x++;
+	y++;
+}
+return (0);
+}
