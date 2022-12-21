0b8ae98a36c0f32d411b966124ce4e732ea0bcef
samecj <chijiokesamuel6@gmail.com>
2022-12-20 20:12:42 -0800

    commited

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: destination
+ * @src: source
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+
+	while (*(src + len) != '\0')
+	{
+		*(dest + len) = *(src + len);
+		len++;
+	}
+	*(dest + len)  = '\0';
+	return (dest);
+}
